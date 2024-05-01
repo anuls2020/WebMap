0f7053cf629913d027e93b0a95ce1588c3daafbf
Owner <anulsasidharan@gmail.com>
2024-05-01 00:11:28 -0400

    Adding multiple pins form file

--- map.py
+++ map.py
@@ -1,13 +1,17 @@
 import folium
+import pandas as pd
 
+data = pd.read_csv("Volcanoes.csv")
+lat = list(data["LAT"])
+lon = list(data["LON"])
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="cartodb positron")
 
 # Adding Future group
 fg = folium.FeatureGroup(name="My Map")
 
 # Adding Marker and change icon colour
-for coordinates in [[38.58, -99.09],[37.58, -97.09]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="I'm a marker", icon=folium.Icon(color='green')))
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="I'm a marker", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
