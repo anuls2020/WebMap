36598b61959c6f80041678a310a5e2b50367937d
Owner <anulsasidharan@gmail.com>
2024-05-01 00:24:44 -0400

    Adding text to popup window.

--- map.py
+++ map.py
@@ -1,17 +1,19 @@
 import folium
 import pandas as pd
 
+# Reading the map data from the csv file
 data = pd.read_csv("Volcanoes.csv")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="cartodb positron")
 
 # Adding Future group
 fg = folium.FeatureGroup(name="My Map")
 
 # Adding Marker and change icon colour
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="I'm a marker", icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
