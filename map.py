5011430e4e97fc26941664b9db9d15233571ed14
Owner <anulsasidharan@gmail.com>
2024-04-30 23:47:39 -0400

    Adding Multiple pins iterate through the list of coordinates

--- map.py
+++ map.py
@@ -6,7 +6,9 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="cartodb positron
 fg = folium.FeatureGroup(name="My Map")
 
 # Adding Marker and change icon colour
-fg.add_child(folium.Marker(location=[38.58, -99.09], popup="I'm a marker", icon=folium.Icon(color='green')))
+for coordinates in [[38.58, -99.09],[37.58, -97.09]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="I'm a marker", icon=folium.Icon(color='green')))
+
 map.add_child(fg)
 
 map.save("Map1.html")
