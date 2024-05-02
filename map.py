82c91372b4947f97820d13d86cfde930d3c4f13b
Owner <anulsasidharan@gmail.com>
2024-05-01 23:10:17 -0400

    Applied color gradients to map pins

--- map.py
+++ map.py
@@ -6,6 +6,17 @@ data = pd.read_csv("Volcanoes.csv")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
+
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="cartodb positron")
 
 # Adding Future group
@@ -13,7 +24,7 @@ fg = folium.FeatureGroup(name="My Map")
 
 # Adding Marker and change icon colour
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color=color_producer(el))))
 
 map.add_child(fg)
 
