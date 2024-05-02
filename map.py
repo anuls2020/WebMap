465844a94deabb191fb90213ffe5d27b1253a13c
Owner <anulsasidharan@gmail.com>
2024-05-02 01:14:59 -0400

    Added circle marker

--- map.py
+++ map.py
@@ -24,7 +24,8 @@ fg = folium.FeatureGroup(name="My Map")
 
 # Adding Marker and change icon colour
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color=color_producer(el))))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el) + " m", fill_color=color_producer(el),
+                                     color='gray', fill_opacity=0.7))
 
 map.add_child(fg)
 
