e92fae267d6a7b7cd7a65ae3cfd036dda0b21256
Owner <anulsasidharan@gmail.com>
2024-05-03 23:07:33 -0400

    Added Layer Control

--- map.py
+++ map.py
@@ -27,10 +27,12 @@ for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el) + " m", fill_color=color_producer(el),
                                      color='gray', fill_opacity=0.7))
 
+# Adding color gradient based on population  to world map
 fg.add_child(folium.GeoJson(data=open("world.json", 'r', encoding='utf-8-sig').read(),
                             style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
                             else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(fg)
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
