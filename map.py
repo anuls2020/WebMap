3ae8164d13de21dd973580ee31c49d62af16c801
Owner <anulsasidharan@gmail.com>
2024-05-03 23:15:37 -0400

    Added Multiple Layers in the maps.

--- map.py
+++ map.py
@@ -19,20 +19,28 @@ def color_producer(elevation):
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="cartodb positron")
 
-# Adding Future group
-fg = folium.FeatureGroup(name="My Map")
+# Adding Future group for Volcanoes
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 # Adding Marker and change icon colour
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el) + " m", fill_color=color_producer(el),
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el) + " m", fill_color=color_producer(el),
                                      color='gray', fill_opacity=0.7))
 
+# Adding Future group for Population
+fgp = folium.FeatureGroup(name="Population")
+
+
 # Adding color gradient based on population  to world map
-fg.add_child(folium.GeoJson(data=open("world.json", 'r', encoding='utf-8-sig').read(),
+fgp.add_child(folium.GeoJson(data=open("world.json", 'r', encoding='utf-8-sig').read(),
                             style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
                             else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fg)
+#Add child to the map object
+map.add_child(fgv)
+map.add_child(fgp)
+
+#Adding layer Control
 map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
